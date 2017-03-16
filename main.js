efae751d06ba24601798d6555dc69687fd761310
HMoralesJr86 <hmoralesjr86@gmail.com>
2017-03-16 17:37:42 -0400

    Homework

---
+++ main.js
@@ -0,0 +1,50 @@
+// // this is the solution to problem 2
+// var football= [["Tom Brady", "Adrian Peterson", "Larry Fitzgerald"], ["Rams", "Browns", "Giants"]];
+//
+// console.log(football[1][0],football[1][2]);
+
+// this is the solution to problem 4
+// var number = prompt ("Enter a Number?")
+//   if (number < 150){
+//     alert("That is less than 150.")
+//   }
+//   else {
+//     (number > 200)
+//     alert("This number " + number + " is greater than 200.")
+//   }
+
+// this is the solution to problem 5
+// var name = prompt ("Enter Your Name")
+//   if (name == "Hector"){
+//     alert("Hi Hector")
+//   } else {
+//     (name === "Hector")
+//     alert("You don't belong here.")
+//   }
+
+// this is the solution to problem 6
+// var enter = prompt("Enter Your Name")
+// function someName(name) {
+//   return(name)
+//
+// }
+// alert(someName(enter))
+// alertName("Hector")
+
+//this is the solution to problem 7
+// function alertName(){
+//   console.log("Hector");
+// }
+// alertName()
+
+//this is the solution to problem 8
+function door(doorNumber) {
+  if (doorNumber == 1) {
+    alert("You win a dollar!")
+  }
+  else if (doorNumber == 2) {
+    alert("You win 2 dollars!")
+  }
+  else if (doorNumber == 3)
+    alert("You win 3 dollars!")
+}
